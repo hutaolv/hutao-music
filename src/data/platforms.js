18eca7e742b1dd6af1ea28b67808a85109a8a75f
1 <1>
2026-07-28 01:15:25 +0800

    feat: 添加咪咕音乐平台支持 (榜单/搜索/播放/歌词)

--- src/data/platforms.js
+++ src/data/platforms.js
@@ -1,9 +1,10 @@
-export const platforms = ['抖音', 'QQ音乐', '网易云音乐', 'B站', '汽水音乐']
+export const platforms = ['抖音', 'QQ音乐', '网易云音乐', 'B站', '汽水音乐', '咪咕音乐']
 
 export const platformColors = {
   '抖音': '#000000',
   'QQ音乐': '#00D26C',
   '网易云音乐': '#D43C33',
   'B站': '#FB7299',
-  '汽水音乐': '#00B4A0'
+  '汽水音乐': '#00B4A0',
+  '咪咕音乐': '#FF6600'
 }
