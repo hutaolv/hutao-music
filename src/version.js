e2d30848b6001c23036bc9e9024a6d82c938209f
hutao <1219615468@qq.com>
2026-08-18 09:55:20 +0800

    chore: 版本号升至 1.0.2

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // APK 当前版本号，需与 android/app/build.gradle 的 versionName 保持一致
 // 发布新版本时同步递增这里、build.gradle 以及服务器 server/downloads/version.json
-export const APP_VERSION = '1.0.1'
+export const APP_VERSION = '1.0.2'
